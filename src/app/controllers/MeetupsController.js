285e1149ef7ee7840b46b7660c098a0efdb77ffb
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2019-06-30 19:21:29 -0300

    Update Meetup

--- src/app/controllers/MeetupsController.js
+++ src/app/controllers/MeetupsController.js
@@ -33,6 +33,47 @@ class MeetupsController {
 
     return res.json(meetup);
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      file_id: Yup.number(),
+      title: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { title, description, location, date, file_id } = req.body;
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'This meetup does not belong to you' });
+    }
+
+    if (date && isBefore(date, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted 2' });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({ error: "Can't update past meetups." });
+    }
+
+    await meetup.update({
+      title,
+      description,
+      location,
+      date,
+      file_id,
+    });
+
+    return res.json(meetup);
+  }
 }
 
 export default new MeetupsController();
--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/meetups', MeetupsController.store);
+routes.put('/meetups/:id', MeetupsController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
